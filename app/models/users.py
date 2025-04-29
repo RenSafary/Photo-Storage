f139e75ff2eeef50a517d1a62ddc31ace90a7738
RenSafary <rensafary@gmail.com>
2025-04-29 19:32:51 +0500

    added table Files

--- app/models/users.py
+++ app/models/users.py
@@ -14,10 +14,15 @@ class Users(BaseModel):
     recover_link = CharField(null=True)
     recover_link_expires = DateTimeField(null=True)
 
+class Files(BaseModel):
+    id = AutoField()
+    user = ForeignKeyField(Users, on_delete="CASCADE")
+    link = CharField()    
+
 
 try:
     db.connect()
-    db.create_tables([Users])
+    db.create_tables([Users, Files])
 except Exception as e:
     print(e)
 finally:
