31741d710d605b5461dec475a6edbd144cd847fb
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-04-30 18:18:15 +0500

    navigation

--- app/routers/main_page.py
+++ app/routers/main_page.py
@@ -2,13 +2,20 @@ from fastapi import APIRouter, Request, Response
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
+from routers.auth.sign_in import verify_token
+
 router = APIRouter()
 
 tmpl = Jinja2Templates(directory="./app/templates")
 
 @router.get("/")
 async def main_page(request: Request):
-    return tmpl.TemplateResponse("main.html", {'request': request})
+    username = verify_token(request)
+    return tmpl.TemplateResponse("main.html",
+    {
+        'request': request,
+        'username': username
+    })
 
 @router.get("/log-out")
 async def log_out(
