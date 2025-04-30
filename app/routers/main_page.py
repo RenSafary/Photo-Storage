212d8a469d885cf2a3c7ebecadb9541ff350ae89
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-04-30 17:19:11 +0500

    log out

--- app/routers/main_page.py
+++ app/routers/main_page.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
@@ -8,3 +9,17 @@ tmpl = Jinja2Templates(directory="./app/templates")
 @router.get("/")
 async def main_page(request: Request):
     return tmpl.TemplateResponse("main.html", {'request': request})
+
+@router.get("/log-out")
+async def log_out(
+    response: Response
+):
+    response = RedirectResponse("/sign-in")
+    response.delete_cookie(
+        key="access_token",
+        path="/",
+        secure=True,
+        httponly=True,
+        samesite="lax"
+    )
+    return response
