a10af97ffbe34ebb442bfdaa939c4054ac00448c
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-04 14:38:24 +0500

    websocket for creating folders

--- app/routers/gallery/gallery.py
+++ app/routers/gallery/gallery.py
@@ -1,22 +1,69 @@
-from fastapi import APIRouter, Request, Form, Cookie, Depends   
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.websockets import WebSocket
 from jose import jwt
+from dotenv import load_dotenv
+import os 
 
 from routers.auth.sign_in import verify_token
+from utils.storage.get_files import get_files
+from models.users import Users, Folders
+
 
 router = APIRouter()
 tmpl = Jinja2Templates(directory="./app/templates/gallery")
 
+def get_current_user(token: str):
+    load_dotenv()
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+
+    payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+    username = payload.get('sub')
+
+    return username
+
 @router.get("/gallery")
 async def files(request: Request):
-    return tmpl.TemplateResponse('gallery.html',{'request': request})
+    files = get_files()
+    return tmpl.TemplateResponse('gallery.html', {'request': request, 'files': files})
 
 
-@router.post("/new-folder")
+@router.websocket("/new-folder/ws")
 async def new_folder(
-    request: Request,
-    new_folder: str = Form(...),
+    websocket: WebSocket
 ):
-    username = verify_token(request)
-    print(username)
-    return
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_json()
+            new_folder = data["new_folder"]
+
+            token = data['access_token']
+            username = get_current_user(token[13::])
+
+            user = Users.get(Users.username == username)
+
+
+            if Folders.get_or_none((Folders.user == user) & (Folders.name == new_folder)):
+                await websocket.send_json(
+                    {
+                        "status":"error",
+                        "details":"Folder name is already taken"
+                    }
+                )
+                #return
+            else: 
+                folder = Folders.create(name=new_folder, user=user.id)
+                await websocket.send_json(
+                    {
+                        "status": "success",
+                        "token": token
+                    }
+                )
+                #return
+    except Exception as e: 
+        print("Error in gallery.py websocket:", e)
+        await websocket.send_json({"status": "error", "detail": "Internal server error"})
+        await websocket.close()
--- static/js/gallery/gallery.js
+++ static/js/gallery/gallery.js
@@ -1,5 +1,35 @@
-$(document).ready(function(){
-    $(".new_folder_button").click(function(){
-        $(".new_folder_form").css({visibility: 'visible'});
-    });
+const ws = new WebSocket("ws://127.0.0.1:8000/new-folder/ws");
+
+const token = document.cookie;
+
+ws.onmessage = function(event) {
+    const response = JSON.parse(event.data);
+    if (response.status === "success") {
+        document.getElementsByName("new_folder")[0].value = "";
+        //ws.close();
+    } else {
+        alert(response.details);
+    }
+};
+
+const form = document.getElementById("new_folder_form"); 
+const button = document.getElementById("new_folder_button"); 
+
+button.addEventListener('click', function() {
+    form.style.visibility = "visible";
+});
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const folder_name = document.getElementsByName("new_folder")[0].value;
+    
+    const data = {
+        new_folder: folder_name,
+        access_token: token
+    }
+    if (ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(data));
+    } else {
+        alert("WebSocket соединение не установлено");
+    }
 });
