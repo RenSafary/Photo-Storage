2be5849917b781fb7e2b97c031f5241885e6b2a1
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-05 12:50:19 +0500

    modified websocket

--- static/js/gallery/gallery.js
+++ static/js/gallery/gallery.js
@@ -1,35 +1,41 @@
-const ws = new WebSocket("ws://127.0.0.1:8000/new-folder/ws");
+let ws = null;
 
-const token = document.cookie;
-
-ws.onmessage = function(event) {
-    const response = JSON.parse(event.data);
-    if (response.status === "success") {
-        document.getElementsByName("new_folder")[0].value = "";
-        //ws.close();
-    } else {
-        alert(response.details);
-    }
-};
-
-const form = document.getElementById("new_folder_form"); 
-const button = document.getElementById("new_folder_button"); 
+const form = document.getElementById("new_folder_form");
+const button = document.getElementById("new_folder_button");
 
 button.addEventListener('click', function() {
-    form.style.visibility = "visible";
+    form.style.visibility = form.style.visibility === 'visible' ? 'hidden' : 'visible';
 });
 
-form.addEventListener('submit', function(event) {
+function sendMessage(event) {
     event.preventDefault();
-    const folder_name = document.getElementsByName("new_folder")[0].value;
     
-    const data = {
-        new_folder: folder_name,
-        access_token: token
-    }
-    if (ws.readyState === WebSocket.OPEN) {
-        ws.send(JSON.stringify(data));
-    } else {
-        alert("WebSocket соединение не установлено");
-    }
-});
+    ws = new WebSocket("ws://127.0.0.1:8000/new-folder/ws");
+    
+    const folder_name = document.getElementsByName("new_folder")[0].value;
+    const token = document.cookie;
+    
+    ws.onopen = function() {
+        ws.send(JSON.stringify({
+            new_folder: folder_name,
+            access_token: token
+        }));
+    };
+    
+    ws.onmessage = function(event) {
+        const response = JSON.parse(event.data);
+        if (response.status === "success") {
+            document.getElementsByName("new_folder")[0].value = "";
+            ws.close();
+            window.location.reload(); 
+        } else {
+            alert(response.details);
+            ws.close();
+        }
+    };
+    
+    ws.onerror = function(error) {
+        alert("Connection error");
+        if (ws) ws.close();
+    };
+}
