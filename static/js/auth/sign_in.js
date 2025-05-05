8ca51e76550505e6478d35088e2ea19e3022bff5
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-05 12:52:16 +0500

    modified websocket

--- static/js/auth/sign_in.js
+++ static/js/auth/sign_in.js
@@ -1,18 +1,7 @@
-const ws = new WebSocket("ws://127.0.0.1:8000/sign-in/ws");
-
-ws.onmessage = function(event) {
-    const response = JSON.parse(event.data);
-    if (response.status === "success") {
-        document.cookie = `access_token=${response.token}; path=/; secure`;
-        ws.close();
-        window.location.href = "/";
-    } else {
-        alert(response.detail);
-    }
-};
-
 function sendMessage(event) {
     event.preventDefault();
+
+    const ws = new WebSocket("ws://127.0.0.1:8000/sign-in/ws");
     
     const username = document.getElementsByName("username")[0].value;
     const password = document.getElementsByName("password")[0].value;
@@ -22,5 +11,18 @@ function sendMessage(event) {
         password: password
     };
     
-    ws.send(JSON.stringify(data));
+    ws.onopen = function() {
+        ws.send(JSON.stringify(data));
+    };
+
+    ws.onmessage = function(event) {
+        const response = JSON.parse(event.data);
+        if (response.status === "success") {
+            document.cookie = `access_token=${response.token}; path=/; secure`;
+            ws.close();
+            window.location.href = "/";
+        } else {
+            alert(response.detail);
+        }
+    };
 }
