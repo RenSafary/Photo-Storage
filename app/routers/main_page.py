18a48a073904a233902f935747a3fdd51177d162
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-05 18:09:54 +0500

    modified redirection

--- app/routers/main_page.py
+++ app/routers/main_page.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request, Response
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from routers.auth.sign_in import verify_token
@@ -8,25 +8,30 @@ router = APIRouter()
 
 tmpl = Jinja2Templates(directory="./app/templates")
 
-@router.get("/")
+
+@router.get("/Photo-Storage")
 async def main_page(request: Request):
     username = verify_token(request)
-    return tmpl.TemplateResponse("main.html",
-    {
-        'request': request,
-        'username': username
-    })
+    if not username:
+        return RedirectResponse("/sign-in")
+    else:
+        return tmpl.TemplateResponse(
+            "main.html", {"request": request, "username": username}
+        )
+
 
 @router.get("/log-out")
-async def log_out(
-    response: Response
-):
-    response = RedirectResponse("/sign-in")
-    response.delete_cookie(
-        key="access_token",
-        path="/",
-        secure=True,
-        httponly=True,
-        samesite="lax"
-    )
-    return response
+async def log_out(request: Request, response: Response):
+    try:
+        user = verify_token(request)
+        if not user:
+            return RedirectResponse("/Photo-Storage")
+        else:
+            response = RedirectResponse("/Photo-Storage")
+            response.delete_cookie(
+                key="access_token", path="/", secure=True, httponly=True, samesite="lax"
+            )
+            return response
+    except Exception as e:
+        print(f"Error in main_page.py: {e}")
+        return RedirectResponse("/Photo-Storage")
