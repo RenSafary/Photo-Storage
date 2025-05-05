1ecd0cb728bf9e0a585ff44f1858056b42340269
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-05 11:22:54 +0500

    modified

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 from routers import main_page
 from routers.auth import sign_in, sign_up, recover_password
-from routers.gallery import gallery, upload_files, folders
+from routers.gallery import gallery, folders
 
 
 app = FastAPI()
@@ -15,7 +15,6 @@ app.include_router(sign_in.router)
 app.include_router(sign_up.router)
 app.include_router(recover_password.router)
 app.include_router(gallery.router)
-app.include_router(upload_files.router)
 app.include_router(folders.router)
 
 
