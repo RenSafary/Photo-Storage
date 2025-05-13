ec9cc93f3e7281260f267421043ebd21d6c3fd9d
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-13 20:13:38 +0500

    ///

--- app/utils/storage/s3_connection.py
+++ app/utils/storage/s3_connection.py
@@ -14,22 +14,3 @@ def connection():
             signature_version='s3v4', 
         )
     )
- 
-"""print('Загрузка файла в бакет')
-s3.upload_file(Filename=FILENAME, Bucket=BUCKET['Name'], Key='hello/hello1.txt')
- 
-print('Список объектов в бакете')
-for obj in s3.list_objects(Bucket=BUCKET['Name']).get('Contents', []):
-    print(obj['Key'])
- 
-print('Чтение файла')
-data = s3.get_object(Bucket=BUCKET['Name'], Key='hello1.txt').get('Body')
-if data is not None:
-    print(data.read())
- 
-print("Скачивание файла")
-s3.download_file(
-    Bucket=BUCKET['Name'],
-    Key='hello1.txt',
-    Filename='./hello2.txt'
-)"""
