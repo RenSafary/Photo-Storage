8cf2259609e7f236b84326c3e38179a66db321a1
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-05-26 17:45:50 +0500

    refactoring

--- app/utils/storage/get_files.py
+++ app/utils/storage/get_files.py
@@ -1,31 +1,37 @@
 import os
 from dotenv import load_dotenv
-from fastapi.responses import JSONResponse
+from typing import Union, List, Dict
 
-from .s3_connection import connection
+from utils.storage.s3_connection import connection
 
-def get_files(username, folder_name):
+def get_files(username: str, links: Union[str, List[str]]) -> List[Dict[str, str]]:
     load_dotenv()
-
     BUCKET_NAME = os.getenv("BUCKET_NAME")
-    BUCKET = {'Name': BUCKET_NAME}
-
     s3 = connection()
-
-    key = f"{username}/{folder_name}"
     
+    files = []
+    
+    if isinstance(links, list):
+        for link in links:
+            files.extend(_get_files(s3, BUCKET_NAME, " ", link.link))
+    else:
+        files = _get_files(s3, BUCKET_NAME, username, links)
+    
+    return files
+
+def _get_files(s3, bucket_name: str, username: str, link: str) -> List[Dict[str, str]]:
     try:
-
-        objects = s3.list_objects_v2(Bucket=BUCKET['Name'], Prefix=key)
-
+        key = str(link) if username == " " else f"{username}/{link}"
+        
+        objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=key)
         files = []
-
+        
         if 'Contents' in objects:
             for file in objects['Contents']:
-                if not file['Key'].endswith('/'):
+                if not file['Key'].endswith('/'): 
                     url = s3.generate_presigned_url(
                         'get_object',
-                        Params={'Bucket': BUCKET['Name'], 'Key':file['Key']},
+                        Params={'Bucket': bucket_name, 'Key': file['Key']},
                         ExpiresIn=3600
                     )
                     files.append({
@@ -33,6 +39,7 @@ def get_files(username, folder_name):
                         'url': url
                     })
         return files
+    
     except Exception as e:
         print(f"Error getting files from S3: {str(e)}")
         return []
