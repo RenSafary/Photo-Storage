91039a406cdb98a07eb74989f6b7f3b7802b1cd5
RenSafary <rensafary@gmail.com>
2025-05-27 17:20:19 +0500

    new field

--- app/models/Files.py
+++ app/models/Files.py
@@ -3,11 +3,13 @@ from peewee import *
 from database import connection
 from models.Folders import Folders
 from models.Tags import Tags
+from models.Users import Users
 
 db = connection()
 
 class Files(Model):
     id = AutoField()
+    user = ForeignKeyField(Users, on_delete="CASCADE")
     folder = ForeignKeyField(Folders, on_delete="CASCADE", null=True)
     link = CharField()
     date_uploaded = DateField()
