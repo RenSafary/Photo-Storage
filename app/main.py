b17ca5b2f772f46fc3f8a597e950aa246d3a44e2
RenSafary <rensafary@gmail.com>
2025-05-27 17:35:21 +0500

    refactoring

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ from routers import main_page
 from routers.auth.sign_in import AuthService
 from routers.auth.recover_password import RecoverPassword
 from routers.auth.sign_up import Sign_Up
-#from routers.gallery.folders import FoldersR
 from routers.gallery.gallery import Gallery
 from routers.tags.find_by_tag import Find_By_Tag
 #from routers.tags.files_by_tag import Files_By_Tag
@@ -31,24 +30,17 @@ recover_password_router = recover_password_service.router
 sign_up_service = Sign_Up()
 sign_up_router = sign_up_service.router
 
-#folders_service = FoldersR()
-#folders_router = folders_service.router
-
 gallery_service = Gallery()
 gallery_router = gallery_service.router
 
 tag_service = Find_By_Tag()
 tag_router = tag_service.router
 
-#tag_files_service = Files_By_Tag()
-#tag_files_router = tag_files_service.router
-
 app.include_router(main_page.router)
 app.include_router(auth_router)
 app.include_router(sign_up_router)
 app.include_router(recover_password_router)
 app.include_router(gallery_router)
-#app.include_router(folders_router)
 app.include_router(tag_router)
 #app.include_router(tag_files_router)
 
