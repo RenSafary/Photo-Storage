7f1761a7ce7112a363c30079efdfeb3e38be29fb
RenSafary <rensafary@gmail.com>
2025-06-05 20:02:50 +0500

    get link and redirect

--- app/go_services/main.go
+++ app/go_services/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"go_services/send_temp_link"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/Photo-Storage/api/send-link", send_temp_link.HandleGetLink).Methods("POST")
+	r.HandleFunc("/gallery/api/share_file/", send_temp_link.HandleGetLink).Methods("POST")
 
-	fmt.Println("Сервер запущен на http://127.0.0.1:8080")
+	fmt.Println("Server started on http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", r)
 }
--- app/go_services/send_temp_link/get_link.go
+++ app/go_services/send_temp_link/get_link.go
@@ -1,17 +1,74 @@
 package send_temp_link
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	//"github.com/gorilla/mux"
 )
 
+type DataForm struct {
+	User     string `form:"user"`
+	Url      string `form:"url"`
+	Platform string `form:"platform"`
+}
+
 func HandleGetLink(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
-	user := r.FormValue("username")
-	urlParam := r.FormValue("url")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	fmt.Println(user, urlParam)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-	w.WriteHeader(http.StatusOK)
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Parsing error", http.StatusBadRequest)
+		return
+	}
+
+	data := DataForm{
+		User:     r.PostFormValue("user"),
+		Url:      r.PostFormValue("url"),
+		Platform: r.PostFormValue("platform"),
+	}
+
+	if data.User == "" || data.Url == "" || data.Platform == "" {
+		http.Error(w, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received: user=%s, url=%s, platform=%s\n", data.User, data.Url, data.Platform)
+
+	shareURL := generateShareURL(data) // поменять на ссылку со своим доменом и в img вставлять её. добавить роутер для этого
+	if shareURL == "" {
+		http.Error(w, "Unsupported platform", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status":   "success",
+		"message":  "File shared successfully",
+		"shareURL": shareURL,
+	})
+}
+
+func generateShareURL(data DataForm) string {
+	var text string = "Hi! You can see my photo!"
+
+	encodedURL := url.QueryEscape(data.Url)
+	encodedText := url.QueryEscape(text)
+
+	switch data.Platform {
+	case "telegram":
+		return fmt.Sprintf("https://t.me/share/url?url=%s&text=%s", encodedURL, encodedText)
+	case "whatsapp":
+		return fmt.Sprintf("https://wa.me/?text=%s%%20%s", encodedText, encodedURL)
+	default:
+		return ""
+	}
 }
