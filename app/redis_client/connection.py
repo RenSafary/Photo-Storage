0b4cf6f56b8b5b78b3aca630e7404d9fcf88eac7
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-06-16 18:55:57 +0500

    test

--- app/redis_client/connection.py
+++ app/redis_client/connection.py
@@ -1,7 +1,21 @@
 from redis import Redis
+import json
 
 
 def connect():
     rd = Redis(host="localhost", port=6379, db=0)
-    print(rd.ping)
+
     return rd
+
+"""
+rd = connect()
+
+cached = rd.get("user_id:1")
+if cached:
+    user_data = json.loads(cached)  
+
+print(user_data["username"])
+
+for file in user_data["files"]:
+    print(file["id"], file["link"])   
+"""
