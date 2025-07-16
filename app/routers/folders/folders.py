bdde8ca76905344a25ee4f3051d2f533fc4b95d4
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-07-16 15:48:29 +0500

    creating folders

--- app/routers/folders/folders.py
+++ app/routers/folders/folders.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from models.Users import Users
@@ -11,8 +11,8 @@ from routers.gallery.gallery import Gallery
 
 from utils.storage.get_files import get_files
 
-from redis_client.models import folders
-from redis_client.models import files
+from redis_client.connection import connect
+from redis_client.models.user import record_user_in_rdb
 
 
 auth = AuthService()
@@ -29,6 +29,15 @@ class FoldersR:
         self.router.add_api_route("/gallery/folders/", self.all_folders, methods=["GET"])
         self.router.add_api_route("/gallery/folders/{username}/{folder}", self.current_folder, methods=["GET"])
         self.router.include_router(gallery_router, prefix="/api")
+        self.router.add_api_route("/gallery/folders/creation", self.create_folder, methods=["POST"])
+
+    def refresh_rdb(self, user: str):
+        user = Users.get(Users.username == user)
+
+        rdb = connect()
+        rdb.delete(f"user_id:{user.id}")
+        record_user_in_rdb(user)
+        print(f"user_id:{user.id} was refreshed")
 
     async def all_folders(self, request: Request):
         user = auth.verify_token(request)
@@ -56,3 +65,25 @@ class FoldersR:
             files = get_files(username, folder)
 
             return self.tmpl.TemplateResponse(request, "folder.html", {"files": files})
+        
+    async def create_folder(
+        self,
+        request: Request,
+        folder_name: str = Form(...),
+    ):
+        # getting user obj
+        username = auth.verify_token(request)
+
+        user = Users.get(Users.username == username)        
+
+        try:
+            # record to db
+            if not Folders.select().where(Folders.user == user.id, Folders.name == folder_name):
+                folder = Folders.create(name=folder_name, user=user.id)
+                folder.save()
+                
+                # refresh redis cache
+                self.refresh_rdb(username)
+        except:
+            print(Exception)
+        return RedirectResponse(url="/gallery/folders", status_code=302)
