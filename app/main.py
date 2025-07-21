60648e2a432ec0991d2181128952fe1975f6ac83
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-07-21 11:46:06 +0500

    reload

--- app/main.py
+++ app/main.py
@@ -51,4 +51,4 @@ if __name__ == "__main__":
     db = connection()
     db.create_tables([Users, Folders, Files, Temp_Link])
     """
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
