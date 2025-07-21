f202279cc07529bf980853810cc68da5a483fc78
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-07-21 17:37:41 +0500

    redis connection

--- app/go_services/redis/connection.go
+++ app/go_services/redis/connection.go
@@ -2,28 +2,29 @@ package redis
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/redis/go-redis/v9"
 )
 
 type RedisCli struct {
 	Client *redis.Client
+	Ctx    context.Context
 }
 
-func RedisClient() (*redis.Client, context.Context) {
-	var ctx = context.Background()
-
+func RedisClient() (*redis.Client, context.Context, error) {
+	ctx := context.Background()
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	return rdb, ctx
-}
-
-func NewRedisDB(rdb *redis.Client) *RedisCli {
-	return &RedisCli{
-		Client: rdb,
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		return nil, nil, fmt.Errorf("redis ping failed: %w", err)
 	}
+
+	log.Println("Redis connected successfully")
+	return rdb, ctx, nil
 }
