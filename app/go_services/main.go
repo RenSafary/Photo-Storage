36170aabeda0f0b9a4fd4c2f1a480d8150217394
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-07-21 19:14:15 +0500

    stories route

--- app/go_services/main.go
+++ app/go_services/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"go_services/send_temp_link"
+	"go_services/stories"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,6 +12,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/gallery/api/share_file/", send_temp_link.HandleGetLink).Methods("POST")
+	r.HandleFunc("/gallery/api/stories/{username}/", stories.Get_Images).Methods("GET")
 
 	fmt.Println("Server started on http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", r)
